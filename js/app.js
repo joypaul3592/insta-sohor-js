917471fbafa36d97f4ff425dc03ebad953810bde
Joy Paul <joypaul3592@gmail.com>
2022-03-11 10:47:54 +0600

    solv the 5th and 6th bug

--- js/app.js
+++ js/app.js
@@ -145,6 +145,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById( "liked" ).innerHTML = '';
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
         const div = createPost(post);
