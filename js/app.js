7de5ca017a8ea0a300e24cfdf0489cfeafa73e97
Joy Paul <joypaul3592@gmail.com>
2022-03-11 10:36:06 +0600

    solv comment issue

--- js/app.js
+++ js/app.js
@@ -51,6 +51,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+    console.log(post)
     const image = post.image;
     const userImage = post.userImage;
     const div = document.createElement( "article" );
@@ -121,9 +122,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
